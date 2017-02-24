f7e453af712cdf5f42a6829ce1e505bce350955c
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-02-24 12:03:12 +0200

    Add footer.php
    footer.php contains the footer of the website. Footer like navigation
    bar is displayed on all pages of the webshop and footer.php file will be
    included in all .php files of the webshop

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php ?>
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title>Registration</title>
+</head>
+<body>
+
+<footer class="footer">
+    <div class="container">
+        <p><h4 class="text-muted">Get to Know Us</h4></p>
+        <p><h5 class="text-muted">Career</h5></p>
+        <p><h5 class="text-muted">About</h5></p>
+    </div>
+</footer>
+
+</body>
+</html>
