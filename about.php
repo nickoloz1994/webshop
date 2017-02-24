00af2b1621cb2c2ee1b97d4e3558093eb517daf4
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-02-24 12:11:12 +0200

    Add about.php
    about.php displays a rief information about the webshop

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<title>About My Store</title>
+</head>
+<body style="background-image: url(img/dev.jpg);">
+
+<?php include('nav.php'); ?>
+
+<div class="jumbotron" style="background: rgba(204,204,204,0); font-weight: bold;">
+	<div style="margin-left: 3%;">
+  		<h1>Hello, world!</h1>
+  		<p><h2>This is my first functional web application</h2></p>
+  		<p><h3>Hope you will enjoy the experience</h3></p>
+	</div>
+</div>
+
+<footer class="footer" style="position: absolute; right: 0;bottom: 0;left: 0;">
+    <div class="container">
+        <p><h4 class="text-muted">Get to Know Us</h4></p>
+        <p><h5 class="text-muted">Career</h5></p>
+        <p><h5 class="text-muted">About</h5></p>
+    </div>
+</footer>
+
+</body>
+</html>
