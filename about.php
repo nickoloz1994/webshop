65bed7586c2e6aba1b6f8001424ec5a3157b35bc
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-02-24 20:01:14 +0200

    Add sticky footer
    For some pages sticky footer is needed. For this reason added new .php
    file with sticky footer code and this file will be included in all the
    pages where needed

--- about.php
+++ about.php
@@ -19,13 +19,7 @@
 	</div>
 </div>
 
-<footer class="footer" style="position: absolute; right: 0;bottom: 0;left: 0;">
-    <div class="container">
-        <p><h4 class="text-muted">Get to Know Us</h4></p>
-        <p><h5 class="text-muted">Career</h5></p>
-        <p><h5 class="text-muted">About</h5></p>
-    </div>
-</footer>
+<?php include('stickyfooter.php'); ?>
 
 </body>
 </html>
---
+++ stickyfooter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title></title>
+</head>
+<body>
+
+<footer class="footer" style="position: absolute; right: 0;bottom: 0;left: 0;">
+    <div class="container">
+        <p><h4 class="text-muted">Get to Know Us</h4></p>
+        <p><h5 class="text-muted">Career</h5></p>
+        <p><h5 class="text-muted">About</h5></p>
+    </div>
+</footer>
+
+</body>
+</html>
