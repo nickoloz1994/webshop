0c992f16aee4fc0ead836615a04908abf1171cc1
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-03-02 18:47:39 +0200

    Correct indentation for improved readability

--- update.php
+++ update.php
@@ -5,24 +5,24 @@ session_start();
 $customer_id = $_SESSION['userid'];
 
 
-	$sql = "INSERT INTO orders(cust_id,`date`) VALUES($customer_id,CURDATE())";
-	mysqli_query($conn,$sql) or die("orders insert failed");
+$sql = "INSERT INTO orders(cust_id,`date`) VALUES($customer_id,DATE_FORMAT(NOW(),'%b %d %Y %h:%i %p')";
+mysqli_query($conn,$sql) or die("orders insert failed");
 
-	$sql = "SELECT MAX(order_id) AS last_id FROM orders";
-	$res = mysqli_query($conn,$sql) or die("select from orders failed");
-	$row = mysqli_fetch_array($res);
-	$id = $row['last_id'];
+$sql = "SELECT MAX(order_id) AS last_id FROM orders";
+$res = mysqli_query($conn,$sql) or die("select from orders failed");
+$row = mysqli_fetch_array($res);
+$id = $row['last_id'];
 
-	$session = $_SESSION['PHPSESSID'];
-	$sql = "SELECT * FROM cart WHERE session_id='".$session."'";
-	$res = mysqli_query($conn,$sql) or die("select from cart failed");
-	while($rows = mysqli_fetch_array($res)){
-		$pr_id = $rows['product_id'];
-		$qty = $rows['quantity'];
+$session = $_SESSION['PHPSESSID'];
+$sql = "SELECT * FROM cart WHERE session_id='".$session."'";
+$res = mysqli_query($conn,$sql) or die("select from cart failed");
+while($rows = mysqli_fetch_array($res)){
+	$pr_id = $rows['product_id'];
+	$qty = $rows['quantity'];
 
-		$sql = "INSERT INTO order_details(order_id,product_id,quantity) VALUES($id,$pr_id,$qty)";
-		mysqli_query($conn,$sql) or die("details insert failed");
-	}
+	$sql = "INSERT INTO order_details(order_id,product_id,quantity) VALUES($id,$pr_id,$qty)";
+	mysqli_query($conn,$sql) or die("details insert failed");
+}
 header("Location:shippingaddress.php");
 
 ?>
