ba4f847d9a41aa1b74e43c1e3b8f28b2166ba2b5
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-03-02 18:45:20 +0200

    Empty cart
    After signing out user cart automatically gets empty

--- logout.php
+++ logout.php
@@ -1,4 +1,13 @@
 <?php
+session_start();
+require('config.php');
+
+$stmt = "SELECT * FROM cart WHERE session_id='".$_SESSION['PHPSESSID']."'";
+$result = mysqli_query($conn,$stmt);
+while ($row = mysqli_fetch_array($result)) {
+	$stmt = "DELETE FROM cart WHERE session_id='".$_SESSION['PHPSESSID']."'";
+	mysqli_query($conn,$stmt);
+}
 
 if (isset($_COOKIE[session_name()])) {
 	setcookie(session_name(),'',time()-50000,'/');
