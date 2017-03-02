746996bf5f3498b189b0b2f7bddbe325173a1a2d
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-03-02 17:28:01 +0200

    Modify shipping address page
    Added phone input and country picker to the page. Also after clicking on
    "Proceed to payment" button shipping information is stored in the
    database

--- shippingaddress.php
+++ shippingaddress.php
@@ -1,3 +1,40 @@
+<?php 
+session_start();
+
+require('config.php');
+
+$cust_id = $_SESSION['userid'];
+if (isset($_POST['submit'])) {
+	$stmt = $conn->prepare(
+	"INSERT INTO shipp_address(
+		`usr_id`,
+		`name`,
+		`address1`,
+		`address2`,
+		`country`,
+		`state`,
+		`city`,
+		`zip`,
+		`phone`)
+	VALUES (?,?,?,?,?,?,?,?,?)");
+
+	$stmt->bind_param("sssssssss",	
+		$cust_id,
+		$_POST['fullname'],
+		$_POST['address1'],
+		$_POST['address2'],
+		$_POST['country2'],
+		$_POST['state'],
+		$_POST['city'],
+		$_POST['zip'],
+		$_POST['phone']);
+
+	$stmt->execute();
+}
+
+$conn->close();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,44 +42,57 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
   	<script type="text/javascript" src="js/bootstrap.js"></script>
   	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+  	<script type="text/javascript" src="plugins/countries.js"></script>
+  	<script type="text/javascript" src="plugins/phone/bootstrap-formhelpers.js"></script>
+  	<script type="text/javascript" src="plugins/phone/bootstrap-formhelpers-phone.en_US.js"></script>
+  	<script type="text/javascript" src="plugins/phone/bootstrap-formhelpers-phone.js"></script>
+  	<link rel="stylesheet" type="text/css" href="plugins/phone/bootstrap-formhelpers.css">
 	<title>Shipping address</title>
 </head>
 <body>
 
 <div class="container-fluid" style="width: 50%;">
 <h1>Shipping address</h1>
-	<form method="post" action="register.php">
+	<form method="post" action="shippingaddress.php">
   		<div class="form-group">
   		  <label>Full Name:</label>
-  		  <input type="text" class="form-control" placeholder="Enter First Name..."		 style="width: 100%" name="fname" required="required">
+  		  <input type="text" class="form-control" placeholder="Enter your name..."		 style="width: 100%" name="fullname" required="required">
   		</div>
   		<div class="form-group">
   		  <label>Address line 1:</label>
-  		  <input type="text" class="form-control" placeholder="Enter Last Name..." style="width: 100%" name="lname" required="required">
+  		  <input type="text" class="form-control" placeholder="Address line 1..." style="width: 100%" name="address1" required="required">
   		</div>
   		<div class="form-group">
   		  <label>Address line 2:</label>
-  		  <input type="text" class="form-control" placeholder="Email" style="width: 100%" name="mail" required="required">
-  		</div>
-  		<div class="form-group">
-  		  <label>City:</label>
-  		  <input type="text" class="form-control" placeholder="Password" style="width: 100%" required="required">
-  		</div>
-  		<div class="form-group">
-  		  <label>State/Province/Region:</label>
-  		  <input type="text" class="form-control" placeholder="Password" style="width: 100%" name="passwd" required="required">
-  		</div>
-  		<div class="form-group">
-  		  <label>ZIP:</label>
-  		  <input type="text" class="form-control" placeholder="Password" style="width: 100%" required="required">
+  		  <input type="text" class="form-control" placeholder="Address line 2" style="width: 100%" name="address2">
   		</div>
   		<div class="form-group">
   		  <label>Country:</label>
-  		  <input type="text" class="form-control" placeholder="Password" style="width: 100%" required="required">
+  		  	<div >
+				<script type= "text/javascript" src = "countries.js"></script>
+				<select id="country2" name ="country2" class="form-control" required="required"></select>
+			</div>
+			<script language="javascript">
+				populateCountries("country2");
+			</script>
+  		</div>
+  		<div class="form-group">
+  		  <label>City:</label>
+  		  <input type="text" class="form-control" placeholder="City..." style="width: 100%" required="required" name="city">
+  		</div>
+  		<div class="form-group">
+  		  <label>State/Province/Region:</label>
+  		  <input type="text" class="form-control" placeholder="State/Province/Region..." style="width: 100%" name="state" required="required">
+  		</div>
+  		<div class="form-group">
+  		  <label>ZIP:</label>
+  		  <input type="text" class="form-control" placeholder="Postal code..." style="width: 100%" required="required" name="zip">
   		</div>
   		<div class="form-group">
   		  <label>Phone number:</label>
-  		  <input type="text" class="form-control" placeholder="Password" style="width: 100%" required="required">
+  		  	<select id="countries_phone1" class="form-control bfh-countries" data-country="US"></select>
+			<br><br>
+			<input type="text" class="form-control bfh-phone" data-country="countries_phone1" name="phone">
   		</div>
   		<button type="submit" class="btn btn-warning" style="margin-left: 2%;margin-top: 0.5%; margin-bottom: 1%; width: 30%;" name="submit">Proceed to payment</button>
 	</form>
