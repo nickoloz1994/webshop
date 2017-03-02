6cd3ec94067a340cc3a1d32c17a5c3938e1a44ef
nickoloz1994 <nika.ptskialadze@gmail.com>
2017-03-02 18:47:04 +0200

    Correct duplicate error
    After adding an item to the cart more than once instead of increasing
    quantity the duplicate entry was being created in the cart. After
    modification of adding code the quantity is changed instead

--- addtocart.php
+++ addtocart.php
@@ -7,38 +7,23 @@ $pr_id = $_POST['id'];
 $qty = $_POST['quantity'];
 $session = $_SESSION['PHPSESSID'];
 
-$sql = "SELECT * FROM cart";
+$sql = "SELECT * FROM cart WHERE session_id='".$session."' AND product_id='".$pr_id."'";
 $result = mysqli_query($conn,$sql);
 $num = mysqli_num_rows($result);
+
 if ($num > 0) {
 	while ($row = mysqli_fetch_array($result)) {
-		if ($row['session_id']==$session && $row['product_id']==$pr_id) {
+		
 			$newqty = $row['quantity'] + $qty;
 			$query = "UPDATE cart SET quantity='".$newqty."' WHERE session_id='".$session."' AND product_id='".$pr_id."'";
 			mysqli_query($conn,$query) or die("Failed");
-		}else{
-			$sql = "INSERT INTO cart (session_id,product_id,quantity) VALUES ('$session',$pr_id,$qty)";
-			mysqli_query($conn,$sql) or die("Failed");
-		}
+		
 	}
 }else{
-	$statemenet = $conn->prepare(
-		"INSERT INTO `cart`(
-			`session_id`,
-			`product_id`,
-			`quantity`) 
-		VALUES (?,?,?)");
-	$statemenet->bind_param("sii",
-		$_SESSION['PHPSESSID'],
-		$_POST['id'],
-		$_POST['quantity']);
-	if($statemenet->execute()){
-		echo "Success";
-	}else{
-		echo $statemenet->errno."###".$statemenet->error;
-	}
+	$sql = "INSERT INTO cart (session_id,product_id,quantity) VALUES ('$session',$pr_id,$qty)";
+	mysqli_query($conn,$sql) or die("Failed");
 }
 
 header("Location:cart.php");
-
+echo $num;
 ?>
